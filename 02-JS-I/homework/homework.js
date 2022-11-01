f8038e6ac5f50f12b14afffa571f225a1c5e1104
Wiston Rondon <wistonrondon8@gmail.com>
2022-11-01 01:34:25 -0300

    second commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola <3";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 55;
 
 // Crea una variable booleana:
 const nuevoBool = null;
