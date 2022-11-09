cfaa389445671f4ba27a014c44a76ef205f45380
Wiston Rondon <wistonrondon8@gmail.com>
2022-11-09 11:41:41 -0300

    commit main branch

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "esto es un texto";
+const nuevaString = "esto es un texto de la brach de main";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 314162;
